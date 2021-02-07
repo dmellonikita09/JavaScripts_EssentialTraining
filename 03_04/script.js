450a9333cceeb7259941a70dfcbbc1105cdf00df
dmellonikita09 <71412989+dmellonikita09@users.noreply.github.com>
2021-02-06 23:00:40 -0800

    Add files via upload

---
+++ 03_04/script.js
@@ -0,0 +1,4 @@
+var a = 5;
+var b = "ducks";
+var sum = a - b;
+console.log(sum);
---
+++ 04_03/script.js
@@ -0,0 +1,9 @@
+function findBiggestFraction(a,b) {
+    a>b ? console.log("a: ", a) : console.log("b: ", b);
+}
+
+var one = 3/4;
+var two = 5/7;
+
+findBiggestFraction(one,two);
+findBiggestFraction(7/16,13/25);
